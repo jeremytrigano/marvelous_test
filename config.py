b277d2af3385628f6e548bc08b7956dab9beda71
Jérémy Trigano <trigano.jeremy@gmail.com>
2019-12-23 14:59:11 +0100

    add config files

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ FB_APP_ID = 802827976844073
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
